a964c83f11373a09fe7968bfd5605ce015a7d981
Eli <elibailey97@gmail.com>
2018-08-01 02:33:41 -0400

    planning

---
+++ app/src/main/java/Team.java
@@ -0,0 +1,25 @@
+
+/**
+ * Created by Elijah Bailey on 8/1/2018.
+ */
+
+public class Team {
+
+    /* My Vision for the Team Feature */
+
+    /* As a user I'd like to add players to my team so I can track stats for them. This will be
+    quicker than scrolling through all Tech's players. If Players are objects rather than just
+    names (Strings) they can be assigned stats. Totals like "Eli had 6 Aces this set" would be
+    great.
+
+    Players are unique, identified by name and number. I'll only need to add players to a team once
+    but players will be accessed often to update or read stats.
+
+    I'd like to present the user with a list of "default players" from which to select their team.
+    That means I'll probably have to add another activity to do the team building. I could also add
+    the option to create and add totally new players to their team. The list will require an a new
+    ArrayAdapter. 
+
+     */
+
+}
